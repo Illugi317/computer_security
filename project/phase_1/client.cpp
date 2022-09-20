145eb66a838fc410069479a3c8bacf3634ad71e1
Illugi Breki Finnsson <illugi@unbreakableunix.com>
2022-09-21 00:34:03 +0200

    Reading in another function and parsing IP&Port correctly

--- project/phase_1/client.cpp
+++ project/phase_1/client.cpp
@@ -3,6 +3,22 @@
 #include <fstream>
 #include <jsoncpp/json/json.h>
 
+Json::Value get_config(char* config_file)
+{
+    // Read config file and throw error if it fails
+    Json::Value config;
+    try
+    {
+        std::ifstream config_stream(config_file);
+        config_stream >> config;
+    }
+    catch (const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+    return config;
+}
+
 int main(int argc, char *argv[])
 {
     // Check if we get the correct number of arguments
@@ -12,14 +28,12 @@ int main(int argc, char *argv[])
     }
     // We got the correct arguments and now try to open the config file.
     std::cout << "Trying to open config file: " << argv[1] << std::endl;
-    try{
-    Json::Value config;                                         // The config object file in memory
-    std::ifstream config_file(argv[1], std::ifstream::binary);  // read the file
-    config_file >> config;                                      // parse the file
-    std::cout << config["server"]["ip"] << std::endl;
-    std::cout << config["server"]["port"] << std::endl;
-    } catch (std::exception &e) {
-        std::cout << "Could not open config file" << std::endl;
-        exit(1);
-    }
+    Json::Value config = get_config(argv[1]);
+    std::cout << "Config file opened successfully" << std::endl;
+    // Get the server address and port from the config file
+    std::string server_address = config["server"]["ip"].asString();
+    int server_port = std::stoi(config["server"]["port"].asCString());
+
+    std::cout << "Server address: " << server_address << std::endl;
+    std::cout << "Server port: " << server_port  << std::endl;
 }
